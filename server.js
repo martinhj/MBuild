0c6057deb07fd18509ecfd4f7fe88cee40d3a544
Martin Holt Juliussen <martinhj@gmail.com>
2018-07-10 23:25:03 +0200

    Refactor, prettier

--- server.js
+++ server.js
@@ -1,52 +1,49 @@
-const io = require('socket.io')()
-const intercept = require('intercept-stdout')
-const client_io = require('socket.io-client')
+const io = require("socket.io")()
+const intercept = require("intercept-stdout")
+const client_io = require("socket.io-client")
+const { removeTrailingNewLine } = require("./lib/remove-trailing-new-line.js")
 
-
-io.on('connection', (socket) => {
-    socket.on('stdout', (msg) => {
-        msg = removeTrailingNewLine(msg)
-        io.sockets.emit(`stdout`, msg)
-    })
-    socket.on('stderr', (msg) => {
-        msg = removeTrailingNewLine(msg)
-        io.sockets.emit(`stderr`, msg)
-    })
+io.on("connection", socket => {
+  socket.on("stdout", msg => {
+    msg = removeTrailingNewLine(msg)
+    io.sockets.emit(`stdout`, msg)
+  })
+  socket.on("stderr", msg => {
+    msg = removeTrailingNewLine(msg)
+    io.sockets.emit(`stderr`, msg)
+  })
 })
 
 io.listen(3334)
 
 const connectClient = () => {
-    /* client_socket.on('disconnect', () => {
-        // console.log('disconnecting...')
-    }) */
+  const client_socket = client_io.connect("http://localhost:3334")
 
-    const client_socket = client_io.connect('http://localhost:3334')
-    // console.log(`got here...`)
+  const unhook_intercept = intercept(
+    txt => {
+      client_socket.emit("stdout", txt)
+      return ""
+    },
+    error_txt => {
+      client_socket.emit("stderr", error_txt)
+      return ""
+    }
+  )
 
-    const unhook_intercept = intercept((txt) => {
-        client_socket.emit('stdout', txt)
-        return ''
-    }, (error_txt) => {
-        client_socket.emit('stderr', error_txt)
-        return ''
+  client_socket.on("disconnect", () => {
+    console.log("disconnecting...")
+  })
+
+  return new Promise((resolve, reject) => {
+    client_socket.on("connect", () => {
+      // TODO: what do we do with reconnects? Collect output until
+      // connection is reetablished (per socket that was connected
+      // earlier) and rerelease the messages on reconnect?
+      // How is this compatible to do a single-run to check if the last
+      // build was ok?
+      resolve(client_socket)
     })
-
-    return new Promise((resolve, reject) => {
-        client_socket.on('connect', () => {
-            // TODO: what do we do with reconnects? Collect output until
-            // connection is reetablished (per socket that was connected
-            // earlier) and rerelease the messages on reconnect?
-            // How is this compatible to do a single-run to check if the last
-            // build was ok?
-            resolve(client_socket)
-        })
-    })
+  })
 }
 
 exports.connectClient = connectClient
-
-const removeTrailingNewLine = (str) => {
-    str = str.replace(/^\s+|\s+$/g, '');
-    return str
-}
